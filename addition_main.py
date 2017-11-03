cdc19fa2157474a8ae45886c528c0f1e9179cbb2
mitchellt9 <mitchell.t9@gmail.com>
2017-11-03 17:23:57 +0530

    debug to True

--- addition_main.py
+++ addition_main.py
@@ -36,5 +36,5 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=True)
 

d001cbe320d6a68062f2ebf5d0d29484127aa8b0
mitchellt9 <mitchell.t9@gmail.com>
2017-11-03 18:10:40 +0530

    Minor code changes
    changed debug to false and included a 500 error page

--- addition_main.py
+++ addition_main.py
@@ -14,8 +14,8 @@ def render_addition():                 #SHOULD HAVE 1 FUNCTION ATLEAST TO RENDER
 def render_result():
     a=int(request.form["value1"]) #Value of the first text box is sent here
     b=int(request.form["value2"])#Value of the second text box is sent here
-    print("Value 1 is ",a)
-    print("Value 2 is ",b)
+    print("Value 1 is ",a) #Will be used to show up in the heroku logs
+    print("Value 2 is ",b) #Will be used to show up in the heroku logs
     result=sum(a,b)               #This is the argument passed and returned to the python based logic file
     return render_template('result.html', value="Adding"+str(a)+" and "+str(b)+ " is "+str(result))            #This return is automatically handled by Flask and redirected to a new page
 
@@ -31,12 +31,18 @@ def render_result():
 def page_not_found(e):
     return render_template('somethingwentwrongpage.html') #redirected to a page I setup for error handling
 
-@app.errorhandler(500)            #This is a Flask inbuild function which handles error 500
+@app.errorhandler(400)            #This is a Flask inbuild function which handles error 500
 def page_not_found(e):
     return "name mismatch" #redirected to a page I setup for error handling
+
+@app.errorhandler(500)            #This is a Flask inbuild function which handles error 500
+def page_not_found(e):
+    return render_template('somethingwentwrongpage.html') #redirected to a page I setup for error handling
+
+
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
 
